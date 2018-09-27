7e9943c860db5f038cb9d9cc5d241723ddd5e039
AppleUser <appleuser@ITANY-IMAC-180.local>
2018-09-27 15:25:15 +0800

    first

---
+++ src/js/01.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log(11);
+console.log(22);
+var a=19;
+function show(username,age){
+	console.log(username);
+	console.log(age);
+}
---
+++ src/js/02.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("需休息");
+var a=20;
